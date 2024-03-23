39b5917202b171109488275796b1bcd641bdc1ef
jaenfigueroa <jaenfigueroa03042000@gmail.com>
2024-03-23 13:12:45 -0500

    update name and username

--- src/helpers/setUser.ts
+++ src/helpers/setUser.ts
@@ -4,7 +4,9 @@ import User from "../types/user"
 export const setUser = (user: User) => {
   $profileImg.src = user.image
   $profileName.textContent = user.name || ''
+  $profileName.href = user.url || ''
   $profileUsername.textContent = `@${user.userName}`  || ''
+  $profileUsername.href = user.url || ''
   $profileDescription.textContent = user.description || 'No hay descripción disponible'
   $profileFollowers.textContent = String(user.followers) || '0'
   $profileFollowing.textContent = String(user.following) || '0'
